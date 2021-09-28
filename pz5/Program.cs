cdcc3924c39541b20c0682021004473f9951f761
Сергей Логинов <sergefa97@mail.ru>
2021-09-28 10:20:06 +0500

    Я сделяль задание

--- pz5/Program.cs
+++ pz5/Program.cs
@@ -6,7 +6,54 @@ namespace pz5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Задание №1
+            Console.WriteLine("Задание №1:");
+            int a;
+            for (a = 10; a <= 100; a += 5)
+            {
+                Console.WriteLine(a);
+            }
+            Console.WriteLine();
+
+            //Зададние №2 
+            Console.WriteLine("Задание №2:");
+            string letters = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"; //Русский алфавит для задания №2
+            for (int b = 0; b < 6; b++)
+            {
+                Console.WriteLine(letters[b]);
+            }
+            Console.WriteLine();
+
+            //Задание №3
+            Console.WriteLine("Задание №3:");
+            for (int c = 0; c < 7; c++)
+            {
+                for (int d = 0; d < 6; d++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            //Задание №4
+            Console.WriteLine("Задание №4:");
+            for (int x = 0; x <= 100; x++)
+            {
+                if (x % 6 == 0)
+                {
+                    Console.Write(x + " ");
+                }
+            }
+            Console.WriteLine();
+
+            //Задание №5
+            Console.WriteLine("Задание №5:");
+            for (int i = 3, j = 50; (i - j) != 29; i++, j--)
+            {
+                Console.WriteLine("{0} {1}", i, j);
+            }
+            Console.WriteLine();
         }
     }
 }
