4fc776631b04ae8eff219f8055e7c00a12218975
Сергей Логинов <sergefa97@mail.ru>
2021-09-29 12:43:29 +0500

    pz6

---
+++ pz6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace pz6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вариант №15:");
+            int a = Convert.ToInt32(Console.ReadLine()); //конвертирование из строки в число
+            double b; //результат
+            b = 0f;
+            int k;
+            k = 1;
+            while(k < a + 1)
+            {
+                b += 1f / k;
+                k++;
+            }
+            Console.WriteLine(b);
+
+        }
+    }
+}
