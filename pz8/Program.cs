96793acbc4efb949d3ec0d7b5ac698d3ecb18990
Сергей Логинов <sergefa97@mail.ru>
2021-10-14 12:02:42 +0500

    pz8

---
+++ pz8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace pz8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[12];
+            Random rnd = new Random();
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = rnd.Next(20, 100);
+                Console.Write(A[i] + " ");
+            }
+            Console.WriteLine();
+
+            int[] B = new int[10];
+
+            int iter_B = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] % 10 == 0)
+                {
+                    B[iter_B] = A[i];
+                    iter_B++;
+                }
+            }
+
+            for (int i = 0; i < B.Length; i++)
+            {
+                Console.WriteLine(B[i] + " ");
+            }
+        }
+    }
+}
