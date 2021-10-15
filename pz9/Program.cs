743ca6b8de7d6ae9cdfb62856223d17c5550b635
Сергей Логинов <sergefa97@mail.ru>
2021-10-15 11:33:08 +0500

    pz9

---
+++ pz9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace pz9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int a = 6; //создание констант
+            const int b = 7;
+            double[,] A = new double[a, b]; //создание массива
+            double count = 0; //создание результата
+            Console.WriteLine("Массив А:");
+            Random rnd = new Random(); //рандом
+            for(int i = 0; i < a; i++) //столбцы
+            {
+                for(int j = 0; j < b; j++) //строки
+                {
+                    A[i, j] = rnd.Next(1, 9) / 10.0; //вывод рандомных чисел
+                    Console.Write($"{A[i,j]}  "); //вывод массива
+                }
+                Console.WriteLine();
+            }
+            for(int i = 0; i < a; i++) //подсчет суммы 4-го столбца
+            {
+                count += A[i, 3];
+            }
+            Console.WriteLine("Сумма чисел 4-го столбца: " + count); //результат
+        }
+    }
+}
