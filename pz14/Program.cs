e04681e473b849a7585aaff1b50cd60ac02234ec
Сергей Логинов <sergefa97@mail.ru>
2021-11-23 20:44:41 +0500

    pz14

---
+++ pz14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace pz14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream file = new FileStream(@"D:\PZ.txt", FileMode.Open, FileAccess.ReadWrite);
+            StreamReader reader = new StreamReader(file);
+            string str = reader.ReadToEnd();
+
+            List<string> strList = new List<string>();
+            strList.AddRange(str.Split("\n"));
+
+            float f = 0f;
+
+            foreach (var item in strList)
+            {
+                f += float.Parse(item.Split(" ")[1]);
+            }
+            Console.WriteLine("Сумма чисел " + f);
+        }
+    }
+}
