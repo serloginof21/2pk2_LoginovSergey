1864aa61f85e8d2973972d8f931869d08a3ca12f
Сергей Логинов <sergefa97@mail.ru>
2022-02-10 13:21:47 +0500

    pz_2.3

---
+++ pz_2.3/ParkingTicket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz2_3
+{
+    class ParkingTicket
+    {
+        public int ID;
+        public int summ;
+        public string startTime;
+        public string endTime;
+        public string carID;
+
+        public ParkingTicket(int _ID, string _startTime, string _endTime, string _carID, int _summ = 100)
+        {
+            ID = _ID;
+            summ = _summ;
+            startTime = _startTime;
+            endTime = _endTime;
+            carID = _carID;
+        }
+
+        public void GetTicketInfo()
+        {
+            Console.WriteLine($"ID = {ID}, summ = {summ}, startTime = {startTime}, endTime = {endTime}, carID = {carID}");
+        }
+    }
+}
---
+++ pz_2.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace pz2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParkingTicket pt1 = new ParkingTicket(8923, "12:33", "2:55", "21342354252341", 39);
+            ParkingTicket pt2 = new ParkingTicket(65645357, "1:34", "22:11", "6576576", 54);
+            ParkingTicket pt3 = new ParkingTicket(2543, "2:22", "6:54", "24542626", 3124);
+            ParkingTicket pt4 = new ParkingTicket(245246, "7:12", "4:12", "98564332");
+
+            pt1.GetTicketInfo();
+            pt2.GetTicketInfo();
+            pt3.GetTicketInfo();
+            pt4.GetTicketInfo();
+        }
+
+    }
+}
