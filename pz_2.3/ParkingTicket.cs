86f3293d45c90b732143110f705e61c48200eeeb
Сергей Логинов <sergefa97@mail.ru>
2022-02-19 16:49:11 +0500

    123

--- pz_2.3/ParkingTicket.cs
+++ pz_2.3/ParkingTicket.cs
@@ -6,26 +6,87 @@ using System.Threading.Tasks;
 
 namespace pz2_3
 {
-    class ParkingTicket
+    public class ParkingTicket
     {
-        public int ID;
-        public int summ;
-        public string startTime;
+        public static readonly int price = 50;
+        static int allSumm = 0;
+        static int count = 0;
+        public static string analize = $"Общее кол-во билетов {count}, а их сумма {allSumm}";
+        public int ID
+        {
+            get
+            {
+                return ID;
+            }
+            set
+            {
+                if (value != 0) ID = value;
+            }
+        }
+        virtual public int summ
+        {
+            get
+            {
+                return (DateTime.Parse(endTime) - DateTime.Parse(startTime)).Hours * price;
+            }
+
+        }
+        public string startTime
+        {
+            get
+            {
+                return startTime;
+            }
+            set
+            {
+                if (DateTime.Parse(value) < DateTime.Parse(endTime))
+                    startTime = value;
+            }
+        }
         public string endTime;
         public string carID;
 
-        public ParkingTicket(int _ID, string _startTime, string _endTime, string _carID, int _summ = 100)
+        public ParkingTicket(int _ID, string _startTime, string _endTime, string _carID)
         {
             ID = _ID;
-            summ = _summ;
             startTime = _startTime;
             endTime = _endTime;
             carID = _carID;
+            count++;
+            allSumm += summ;
         }
-
-        public void GetTicketInfo()
+        ~ParkingTicket()
         {
+            allSumm -= summ;
+            count--;
+        }
+        public virtual void GetTicketInfo()
+        {
+
             Console.WriteLine($"ID = {ID}, summ = {summ}, startTime = {startTime}, endTime = {endTime}, carID = {carID}");
         }
     }
+    class SpecialParkingTicket : ParkingTicket
+    {
+        public int discount;
+
+        public override int summ
+        {
+            get
+            {
+                return (DateTime.Parse(endTime) - DateTime.Parse(startTime)).Hours * price * discount / 100;
+            }
+
+        }
+        SpecialParkingTicket(int _ID, string _startTime, string _endTime, string _carID, int _discount) : base(_ID, _startTime, _endTime, _carID)
+        {
+            discount = _discount;
+        }
+        public override void GetTicketInfo()
+        {
+            Console.WriteLine($"ID = {ID}, summ = {summ}, startTime = {startTime}, endTime = {endTime}, carID = {carID},  discount ={ discount}");
+        }
+    }
 }
+
+
--- pz_2.3/Program.cs
+++ pz_2.3/Program.cs
@@ -6,10 +6,10 @@ namespace pz2_3
     {
         static void Main(string[] args)
         {
-            ParkingTicket pt1 = new ParkingTicket(8923, "12:33", "2:55", "21342354252341", 39);
-            ParkingTicket pt2 = new ParkingTicket(65645357, "1:34", "22:11", "6576576", 54);
-            ParkingTicket pt3 = new ParkingTicket(2543, "2:22", "6:54", "24542626", 3124);
-            ParkingTicket pt4 = new ParkingTicket(245246, "7:12", "4:12", "98564332");
+            ParkingTicket pt1 = new ParkingTicket(8923, "12", "14", "21342354252341");
+            ParkingTicket pt2 = new ParkingTicket(65645357, "1", "2", "6576576");
+            ParkingTicket pt3 = new ParkingTicket(2543, "2", "6", "24542626");
+            ParkingTicket pt4 = new ParkingTicket(245246, "7", "13", "98564332");
 
             pt1.GetTicketInfo();
             pt2.GetTicketInfo();
