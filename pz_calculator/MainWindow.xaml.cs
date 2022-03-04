ba2f3c6f71bfecd61f59fc2d5b95793923ea587f
Сергей Логинов <sergefa97@mail.ru>
2022-03-04 22:15:19 +0500

    calculate

---
+++ pz_calculator/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+
+namespace pz_2_006_7_
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (UIElement el in Buttons.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button)el).Click += ButtonClick;
+                }
+            }
+        }
+        private void ButtonClick(Object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string textButton = ((Button)e.OriginalSource).Content.ToString();
+
+                if (textButton == "=")
+                {
+                    Result.Text = new DataTable().Compute(Result.Text, null).ToString();
+                }
+                else Result.Text += textButton;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+}
