0e58e4d556ac4f7b40af35404cac7d6a9243e5ea
Сергей Логинов <sergefa97@mail.ru>
2022-04-21 21:01:40 +0500

    PZ_sobitiya

---
+++ PZ_9_Sobitiya/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public delegate void Delegate();
+
+namespace PZ_9_Sobitiya
+{
+    public class Counter
+    {
+        public int num;
+        public Counter(int x)
+        {
+            num = x;
+        }
+
+        public void Numbers()
+        {
+            Cutch a = new Cutch();
+            for (int i = 1; i < 1001; i++)
+            {
+                a.ActiveateEvent(i);
+
+                if (i == 800) break;
+            }
+        }
+    }
+    public class Cutch
+    {
+        public event Delegate num_get;
+        public void ActiveateEvent(int now)
+        {
+
+            if (now == 200)
+            {
+                num_get = Dvesti;
+            }
+            else if (now == 800)
+            {
+                num_get = Vosemsot;
+            }
+            else num_get = null;
+            if (num_get != null) num_get();
+        }
+        void Dvesti()
+        {
+            Console.WriteLine("Вот ты и попался друг, 200");
+        }
+        void Vosemsot()
+        {
+            Console.WriteLine("Как рулить?! 800");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter b = new Counter(1);
+            b.Numbers();
+        }
+    }
+}
+
